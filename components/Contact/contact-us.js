0fb7c6a1fc2a780ce7c7e513e53b5adbb8c15ca8
AryanDhamale <aryandhamale07@gmail.com>
2025-05-10 12:53:49 +0530

    fixed colan

--- components/Contact/contact-us.js
+++ components/Contact/contact-us.js
@@ -24,7 +24,7 @@ function Contact() {
                     <div className="flex items-center justify-center">
                         {/* bg-[#ffb800] */}
                         <div className=" w-[65%] flex flex-col gap-y-6 border-2  border-slate-300 dark:border-slate-800 rounded-3xl px-6 py-8">
-                            <h2 className='font-medium text-sm'> <span className=" text-black dark:text-white">Say, Hello</span>  <span className="text-red-500 font-medium">[ the form it's not working yet ]</span> </h2>
+                            <h2 className='font-medium text-sm'> <span className=" text-black dark:text-white">Say, Hello</span>  <span className="text-red-500 font-medium">[ the form it&apos;s not working yet ]</span> </h2>
                             <div>
                             <input className='border px-5 py-3 rounded-md outline-0 bg-white dark:bg-transparent w-full' type="text" placeholder="Enter your name" />
                             </div>
